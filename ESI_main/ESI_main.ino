3a33bb1d9c065dbba8f370a1c21ce6d6e618ad87
Aymericfabre <94149458+Aymericfabre@users.noreply.github.com>
2023-09-29 17:19:23 +1000

    Add files via upload

--- ESI_main/ESI_main.ino
+++ ESI_main/ESI_main.ino
@@ -1,4 +1,3 @@
-
 // necessary libraries
 #include <stdbool.h>
 #include <math.h>
@@ -22,9 +21,11 @@ const int ldrPin = A1;
 const float ldrThreshold = 200.0;
 
 // LED & Buzzer variables
-const int ledPin = 13;
-const int buzzPin = 3;
-
+const int redPin = 7;
+const int greenPin = 6;
+const int bluePin = 5;
+const int buzzPin = 4
+;
 // LCD variables
 const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 
@@ -51,6 +52,7 @@ void loop() {
   float curr_temp = get_temp();           // get current temperature (deg C)
   float curr_humidity = get_humidity();   // get current humidity (%)
   bool is_day = get_light();              // get light or day (bool)
+  buzz_LED(co2_ppm);			  // update buzzer and led
 }
 
 // returns: current temperature in degrees celcius
@@ -108,16 +110,33 @@ bool get_light() {
   }
 }
 
-// void buzz_LED(float co2_ppm) {
-//   // Linear map of CO2 concentration in the air
-//   // LED green blinking for good air, then to orange for mid air, red for poor air
-//   // Buzzer will only buzz if air is mid or worse, buzz faster and higher pitch for poorer air
+void buzz_LED(float co2_ppm) {
+  // Linear map of CO2 concentration in the air
+  // LED green blinking for good air, then to orange for mid air, red for poor air
+  // Buzzer will only buzz if air is mid or worse, buzz faster and higher pitch for poorer air
 
-// }
+  // CAN GET RID OF BLUE PIN SINCE IT'S ALWAYS ZEROOOOOOOOOOOO
 
+  if (co2_ppm < 300) {
+    // Green
+    setColour(0, 255, 0);
 
+  } else if (co2_ppm >= 300 && co2_ppm <= 700) {
+    // Orange
+    setColour(255, 165, 0);
 
+  } else if (co2_ppm > 700) {
+    // Red
+    setColour(255, 0, 0);
 
+  }
 
+  int toneFreq = map(co2_ppm, 10, 1000, 31, 15000);
+  tone(buzzPin, toneFreq);
+}
 
-
+void setColour (int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
