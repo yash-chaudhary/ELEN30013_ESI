47f2d73308cb86622467d81d33d62eb11a6fc70e
Aymericfabre <94149458+Aymericfabre@users.noreply.github.com>
2023-10-13 22:02:56 +1100

    Add files via upload

--- ESI_main/ESI_main.ino
+++ ESI_main/ESI_main.ino
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <math.h>
 #include <dht.h>            // include the library http://arduino.cc/playground/Main/DHTLib
-#include <LiquidCrystal.h>  // include the library https://www.arduino.cc/en/Reference/LiquidCrystal
 #include "MQ135.h"
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
@@ -12,10 +11,8 @@
 #define DHT11_PIN 3
 
 // software serial
-const int rxPin = 6;
-const int txPin = 7;
-
-// Set up a new SoftwareSerial object
+const int rxPin = 7;
+const int txPin = 8;
 SoftwareSerial mySerial (rxPin, txPin);
 
 // MQ135 Sensor
@@ -31,7 +28,10 @@ ThreeWire myWire(dat_pin,clk_pin,rst_pin); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
 
 // thermistor variables
+dht DHT;
+float curr_humidity;
 int thermistorPin = A4;
+float curr_temp;
 float Vo;
 float Rt;
 float T;
@@ -42,18 +42,21 @@ float B = 2200.0;
 // LDR variables
 const int ldrPin = A0;
 const float ldrThreshold = 200.0;
+bool is_day;
 
 // LED & Buzzer variables
 const int redPin = A3;
 const int greenPin = A2;
 const int buzzPin = 4;
 
-// LCD variables
-const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-
-// instantiations
-dht DHT;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// Switch variables
+const int switchPin1 = 5;
+const int switchPin2 = 6;
+int state1;
+int state2;
+unsigned long currentMillis = 0;
+unsigned long previousBuzzMillis = 0;
+unsigned long previousPacketMillis = 0;
 
 // function prototypes
 int get_co2_ppm();
@@ -68,10 +71,14 @@ void buzz_LED(float co2_ppm);
 void setup() {
   Serial.begin(9600);   // 9600 bits per second baud rate
   mySerial.begin(9600); // start software serial
+
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(buzzPin, OUTPUT);
   pinMode(ldrPin, INPUT);
+
+  pinMode(switchPin1, INPUT_PULLUP);
+  pinMode(switchPin2, INPUT_PULLUP);
 
   Rtc.Begin();
   RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
@@ -81,19 +88,93 @@ void setup() {
 
 // main loop
 void loop() {
-  co2_ppm = get_co2_ppm();
-  RtcDateTime curr_time = get_time();
-  float curr_temp = get_temp();           // get current temperature (deg C)
-  float curr_humidity = get_humidity();   // get current humidity (%)
-  bool is_day = get_light();              // get light or day (bool)
-  co2_ppm = analogRead(ldrPin);
-  buzz_LED(co2_ppm);			                // update buzzer and led
+  currentMillis = millis();
 
-  int toneDuration = map(co2_ppm, 5, 1000, 1000, 10);
-  Serial.print("Buzz Duration: ");
-  Serial.println(toneDuration);
+  state1 = digitalRead(switchPin1);
+  state2 = digitalRead(switchPin2);
 
-  delay(toneDuration);
+  if (state1 == HIGH && state2 == HIGH) // Demo mode
+  {
+    run_demo();
+  }
+  else if (state1 == LOW) // Normal operation
+  {
+    RtcDateTime curr_time = get_time();
+    co2_ppm = get_co2_ppm();
+    curr_temp = get_temp();            // get current temperature (deg C)
+    curr_humidity = get_humidity();    // get current humidity (%)
+    is_day = get_light();              // get light or day (bool)
+    co2_ppm = analogRead(ldrPin);
+    buzz_LED(co2_ppm);			           // update buzzer and led
+
+    if (currentMillis - previousPacketMillis >= 1000) {
+      String packet = String(demo_ppm) + "," + String(demo_temp) + "," + String(demo_humidity) + "," + String(demo_is_day);
+      mySerial.println(packet);
+      previousPacketMillis = millis();
+    }
+
+  }
+  else if (state2 == LOW) // Off mode
+  {
+    setColour(0, 0);
+    Serial.println("Switch is in the OFF position");
+    //delay(500);
+  }
+  
+}
+
+void run_demo() {
+
+  int demo_ppm = 0;
+  float demo_temp = 0;
+  int demo_humidity = 0;
+  bool demo_is_day = true;
+  
+  while (state1 == HIGH && state2 == HIGH) {
+    buzz_LED(demo_ppm);	  // update buzzer and led
+
+    currentMillis = millis();
+    if (currentMillis - previousPacketMillis >= 1000) {
+  
+      if (demo_ppm == 1000) {
+        demo_ppm = 0;
+        demo_temp = 0;
+        demo_humidity = 0;
+        demo_is_day = !demo_is_day;
+      }
+
+      demo_ppm += 10;
+      demo_temp += 0.5;
+      demo_humidity += 1;
+
+      RtcDateTime now = Rtc.GetDateTime();
+
+      Serial.print("time delta: ");
+      Serial.println(currentMillis - previousPacketMillis);
+
+      Serial.print("demo_is_day: ");
+      Serial.println(demo_is_day);
+      Serial.print("demo_ppm: ");
+      Serial.println(demo_ppm);
+      Serial.print("Temperature reading (C): ");
+      Serial.println(demo_temp);
+      Serial.print("Humidity reading (%): ");
+      Serial.println(demo_humidity);
+      printDateTime(now);
+
+      // Convert demo variables to ASCII characters and create a packet
+      String packet = String(demo_ppm) + "," + String(demo_temp) + "," + String(demo_humidity) + "," + String(demo_is_day);
+
+      // Send the packet through mySerial
+      Serial.print("packet: ");
+      Serial.println(packet);
+
+      previousPacketMillis = millis();
+    }
+
+    state1 = digitalRead(switchPin1);
+    state2 = digitalRead(switchPin2);
+  }
 }
 
 // returns: MQ135 CO2 concentration in ppm
@@ -168,12 +249,10 @@ bool get_light() {
   if (ldrReading <= ldrThreshold) {
     Serial.print("It's NIGHT: ");
     Serial.println(ldrReading);
-    mySerial.println(ldrReading);
     return false;
   } else {
     Serial.print("It's DAY: ");
     Serial.println(ldrReading);
-    mySerial.println(ldrReading);
     return true;
   }
 }
@@ -196,22 +275,18 @@ void buzz_LED(float co2_ppm) {
     setColour(255, 0);
   }
 
-  // int greenValue = map(co2_ppm, 10, 1000, 255, 128);
-  // int redValue = map(co2_ppm, 10, 1000, 128, 255);
-  // Serial.print("greenValue: ");
-  // Serial.println(greenValue);
-  // Serial.print("redValue: ");
-  // Serial.println(redValue);
-  // analogWrite(redPin, redValue);
-  // analogWrite(greenPin, greenValue);
-
-  // setColour(redValue, greenValue, 0);
-
   int toneFreq = map(co2_ppm, 5, 1000, 31, 1800);
-  Serial.print("Buzz Freq: ");
-  Serial.println(toneFreq);
+  // Serial.print("Buzz Freq: ");
+  // Serial.println(toneFreq);
 
-  tone(buzzPin, toneFreq, 100);
+  int toneDuration = map(co2_ppm, 5, 1000, 1000, 10);
+  // Serial.print("Buzz Duration: ");
+  // Serial.println(toneDuration);
+
+  if (currentMillis - previousBuzzMillis >= toneDuration) {
+    tone(buzzPin, toneFreq, 100);
+    previousBuzzMillis = millis();
+  }
 }
 
 void setColour (int redValue, int greenValue) {
