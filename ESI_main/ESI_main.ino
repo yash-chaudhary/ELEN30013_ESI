f8f524f6112b562ac2df7fab3d550b5215db8430
Aymericfabre <94149458+Aymericfabre@users.noreply.github.com>
2023-10-14 20:07:33 +1100

    Add files via upload

--- ESI_main/ESI_main.ino
+++ ESI_main/ESI_main.ino
@@ -16,7 +16,7 @@ const int txPin = 8;
 SoftwareSerial mySerial (rxPin, txPin);
 
 // MQ135 Sensor
-int co2_ppm;
+int air_ppm;
 int sensorPinAnalogue = A1;
 MQ135 gasSensor(sensorPinAnalogue, 200);
 
@@ -59,16 +59,22 @@ unsigned long previousBuzzMillis = 0;
 unsigned long previousPacketMillis = 0;
 
 // function prototypes
-int get_co2_ppm();
-RtcDateTime get_time();
+void run_normal();
+void run_demo();
+void printSerial(int air_ppm, float curr_temp, int curr_humidity, bool is_day);
+int get_air_ppm();
 float get_temp();
-float fet_humidity();
+float get_humidity();
 bool get_light();
-void buzz_LED(float co2_ppm);
-
+void buzz_LED(int air_ppm);
+void setColour (int redValue, int greenValue);
 
 // setup
 void setup() {
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  Rtc.SetDateTime(compiled);
+
   Serial.begin(9600);   // 9600 bits per second baud rate
   mySerial.begin(9600); // start software serial
 
@@ -80,15 +86,16 @@ void setup() {
   pinMode(switchPin1, INPUT_PULLUP);
   pinMode(switchPin2, INPUT_PULLUP);
 
-  Rtc.Begin();
-  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-  Rtc.SetDateTime(compiled);
+  for (int freq = 500; freq <= 1000; freq += 10) {
+    tone(buzzPin, freq, 100);
+    delay(5);
+  }
+  delay(1000);
 }
 
 
 // main loop
 void loop() {
-  currentMillis = millis();
 
   state1 = digitalRead(switchPin1);
   state2 = digitalRead(switchPin2);
@@ -99,31 +106,58 @@ void loop() {
   }
   else if (state1 == LOW) // Normal operation
   {
-    RtcDateTime curr_time = get_time();
-    co2_ppm = get_co2_ppm();
-    curr_temp = get_temp();            // get current temperature (deg C)
-    curr_humidity = get_humidity();    // get current humidity (%)
-    is_day = get_light();              // get light or day (bool)
-    co2_ppm = analogRead(ldrPin);
-    buzz_LED(co2_ppm);			           // update buzzer and led
-
-    if (currentMillis - previousPacketMillis >= 1000) {
-      String packet = String(demo_ppm) + "," + String(demo_temp) + "," + String(demo_humidity) + "," + String(demo_is_day);
-      mySerial.println(packet);
-      previousPacketMillis = millis();
-    }
-
+    run_normal();
   }
   else if (state2 == LOW) // Off mode
   {
     setColour(0, 0);
     Serial.println("Switch is in the OFF position");
-    //delay(500);
+    delay(500);
   }
   
 }
 
+void run_normal() {
+
+  tone(buzzPin, 1000, 100);
+  delay(200);
+  tone(buzzPin, 1000, 100);
+  delay(1000);
+
+  while (state1 == LOW) {
+    buzz_LED(air_ppm);	  // update buzzer and led
+
+    currentMillis = millis();
+    if (currentMillis - previousPacketMillis >= 500) {
+      Serial.print("time delta: ");
+      Serial.println(currentMillis - previousPacketMillis);
+
+      RtcDateTime curr_time = Rtc.GetDateTime();
+      air_ppm = get_air_ppm();
+      curr_temp = get_temp();            // get current temperature (deg C)
+      curr_humidity = get_humidity();    // get current humidity (%)
+      is_day = get_light();              // get light or day (bool)
+
+      air_ppm = analogRead(ldrPin);
+  
+      printSerial(air_ppm, curr_temp, curr_humidity, is_day);
+
+      previousPacketMillis = millis();
+    }
+
+    state1 = digitalRead(switchPin1);
+    state2 = digitalRead(switchPin2);
+  }
+}
+
 void run_demo() {
+
+  tone(buzzPin, 500, 100);
+  delay(200);
+  tone(buzzPin, 1000, 100);
+  delay(200);
+  tone(buzzPin, 1500, 100);
+  delay(1000);
 
   int demo_ppm = 0;
   float demo_temp = 0;
@@ -134,7 +168,9 @@ void run_demo() {
     buzz_LED(demo_ppm);	  // update buzzer and led
 
     currentMillis = millis();
-    if (currentMillis - previousPacketMillis >= 1000) {
+    if (currentMillis - previousPacketMillis >= 500) {
+      Serial.print("time delta: ");
+      Serial.println(currentMillis - previousPacketMillis);
   
       if (demo_ppm == 1000) {
         demo_ppm = 0;
@@ -143,31 +179,11 @@ void run_demo() {
         demo_is_day = !demo_is_day;
       }
 
-      demo_ppm += 10;
-      demo_temp += 0.5;
-      demo_humidity += 1;
+      demo_ppm += 20;
+      demo_temp += 1;
+      demo_humidity += 2;
 
-      RtcDateTime now = Rtc.GetDateTime();
-
-      Serial.print("time delta: ");
-      Serial.println(currentMillis - previousPacketMillis);
-
-      Serial.print("demo_is_day: ");
-      Serial.println(demo_is_day);
-      Serial.print("demo_ppm: ");
-      Serial.println(demo_ppm);
-      Serial.print("Temperature reading (C): ");
-      Serial.println(demo_temp);
-      Serial.print("Humidity reading (%): ");
-      Serial.println(demo_humidity);
-      printDateTime(now);
-
-      // Convert demo variables to ASCII characters and create a packet
-      String packet = String(demo_ppm) + "," + String(demo_temp) + "," + String(demo_humidity) + "," + String(demo_is_day);
-
-      // Send the packet through mySerial
-      Serial.print("packet: ");
-      Serial.println(packet);
+      printSerial(demo_ppm, demo_temp, demo_humidity, demo_is_day);
 
       previousPacketMillis = millis();
     }
@@ -177,67 +193,74 @@ void run_demo() {
   }
 }
 
-// returns: MQ135 CO2 concentration in ppm
-int get_co2_ppm() {
-  co2_ppm = gasSensor.getPPM(); // Read analog input pin A5
-  
-  if (co2_ppm < 5) {
-    co2_ppm = 5;
-  } else if (co2_ppm > 1000) {
-    co2_ppm = 1000;
-  }
-  
-  Serial.print("co2_ppm: ");
-  Serial.println(co2_ppm);
-  //delay(1000);
+void printSerial(int air_ppm, float curr_temp, int curr_humidity, bool is_day) {
 
-  return co2_ppm;
+  RtcDateTime curr_time = Rtc.GetDateTime();
+
+  Serial.print("air_ppm: ");
+  Serial.println(air_ppm);
+  Serial.print("Temperature reading (C): ");
+  Serial.println(curr_temp);
+  Serial.print("Humidity reading (%): ");
+  Serial.println(curr_humidity);
+  Serial.print("is_day: ");
+  Serial.println(is_day);
+
+  char datestring[11];
+  char timestring[9];
+
+  snprintf_P(datestring, 
+          11,
+          PSTR("%02u/%02u/%04u"),
+          curr_time.Day(),
+          curr_time.Month(),
+          curr_time.Year() );
+
+  snprintf_P(timestring, 
+          9,
+          PSTR("%02u:%02u:%02u"),
+          curr_time.Hour(),
+          curr_time.Minute(),
+          curr_time.Second() );
+
+  // Convert demo variables to ASCII characters and create a packet
+  String packet = String(air_ppm) + "," + String(curr_temp) + "," + String(curr_humidity) + "," + String(is_day) + "," + String(datestring) + "," + String(timestring);
+
+  // Send the packet through mySerial
+  mySerial.println(packet);
+
+  Serial.print("packet: ");
+  Serial.println(packet);
 }
 
-RtcDateTime get_time() {
-  RtcDateTime now = Rtc.GetDateTime();
+// returns: MQ135 sensor concentration in ppm
+int get_air_ppm() {
+  air_ppm = gasSensor.getPPM(); // Read analog input pin A5
+  
+  if (air_ppm < 5) {
+    air_ppm = 5;
+  } else if (air_ppm > 1000) {
+    air_ppm = 1000;
+  }
 
-  printDateTime(now);
-  Serial.println();
+  return air_ppm;
 }
 
 // returns: current temperature in degrees celcius
 float get_temp() {
   Vo = analogRead(thermistorPin);
   Vo = Vo * 5 / 1024;
-  
-  // Serial.print("Voltage reading (V): ");
-  // Serial.println(Vo);
-
   Rt = Vo * Ro / (5 - Vo);
 
-  // Serial.print("Resistance reading (Ohms): ");
-  // Serial.println(Rt);
-
-  int chk = DHT.read11(DHT11_PIN);
-  float T_DHT = DHT.temperature;
-
-  // For DHT11 calibration
-  // B = 1.0 * log(Rt / Ro) / (1 / (273 + T_DHT) - 1 / (273 + To));
-  // Serial.print("B reading: ");
-  // Serial.println(B);
-
   T = 1.0 / (1.0 / B * log(Rt / Ro) + 1 / (273 + To)) - 273;
-
-  Serial.print("Temperature reading (C): ");
-  Serial.println(T);
-  Serial.println();
 
   return T;
 }
 
 // returns: current temperature as a % of water vapor in air
 float get_humidity() {
-
   int chk = DHT.read11(DHT11_PIN);
   float H_DHT = DHT.humidity;
-  Serial.print("Humidity reading (%): ");
-  Serial.println(H_DHT);
 
   return H_DHT;
 }
@@ -247,39 +270,37 @@ bool get_light() {
   int ldrReading = analogRead(ldrPin);
   
   if (ldrReading <= ldrThreshold) {
-    Serial.print("It's NIGHT: ");
-    Serial.println(ldrReading);
+    //Serial.print("It's NIGHT: ");
     return false;
   } else {
-    Serial.print("It's DAY: ");
-    Serial.println(ldrReading);
+    // Serial.print("It's DAY: ");
     return true;
   }
 }
 
-void buzz_LED(float co2_ppm) {
-  // Linear map of CO2 concentration in the air
+void buzz_LED(int air_ppm) {
+  // Linear map of sensor concentration in the air
   // LED green blinking for good air, then to orange for mid air, red for poor air
-  // Buzzer will only buzz if air is mid or worse, buzz faster and higher pitch for poorer air
+  // Buzzer will, buzz faster and higher pitch for poorer air
 
-  if (co2_ppm < 400) {
+  if (air_ppm < 400) {
     // Green
     setColour(0, 255);
 
-  } else if (co2_ppm >= 400 && co2_ppm <= 700) {
+  } else if (air_ppm >= 400 && air_ppm <= 700) {
     // Orange
     setColour(255, 165);
 
-  } else if (co2_ppm > 700) {
+  } else if (air_ppm > 700) {
     // Red
     setColour(255, 0);
   }
 
-  int toneFreq = map(co2_ppm, 5, 1000, 31, 1800);
+  int toneFreq = map(air_ppm, 5, 1000, 31, 1800);
   // Serial.print("Buzz Freq: ");
   // Serial.println(toneFreq);
 
-  int toneDuration = map(co2_ppm, 5, 1000, 1000, 10);
+  int toneDuration = map(air_ppm, 5, 1000, 1000, 100);
   // Serial.print("Buzz Duration: ");
   // Serial.println(toneDuration);
 
@@ -292,27 +313,4 @@ void buzz_LED(float co2_ppm) {
 void setColour (int redValue, int greenValue) {
   analogWrite(redPin, redValue);
   analogWrite(greenPin, greenValue);
-}
-
-void printDateTime(const RtcDateTime& dt)
-{
-    char datestring[11];
-    char timestring[9];
-
-    snprintf_P(datestring, 
-            11,
-            PSTR("%02u/%02u/%04u"),
-            dt.Month(),
-            dt.Day(),
-            dt.Year() );
-
-    snprintf_P(timestring, 
-            9,
-            PSTR("%02u:%02u:%02u"),
-            dt.Hour(),
-            dt.Minute(),
-            dt.Second() );
-
-    Serial.println(datestring);
-    Serial.println(timestring);
 }
